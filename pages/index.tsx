9c38a077b6db54a6429ece228bf9ee86b97e5c89
Agustin Daniel Messina <agustindmessina@gmail.com>
2022-12-02 07:28:12 -0300

    pokemon api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,24 @@
-import { NextPage } from "next";
+import { NextPage, GetStaticProps } from "next";
 import { Layout } from '../components/layouts';
-import { GetStaticProps } from 'next'
-import { pokeApi } from "../api";
 
-const HomePage: NextPage = () => {
+
+import { pokeApi } from "../api";
+import { PokemonListResponse, SmallPokemon } from "../interfaces";
+
+interface Porps {
+	pokemons: SmallPokemon[];
+}
+
+const HomePage: NextPage<Porps> = ({ pokemons }) => {
+
+	console.log(pokemons);
+	
+
+	
 	return (
 		<Layout title="Listado de pokemos">
 			<ul>
-				
+				<li>1</li>
 			</ul>
 		</Layout>
 	);
@@ -22,9 +33,13 @@ const HomePage: NextPage = () => {
 
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-	const { data } = await  pokeApi.get('/pokemon?limit=151');
-
+	const { data } = await  pokeApi.get<PokemonListResponse>('/pokemon?limit=151', {
+		headers: {
+			'accept-encoding': '*'
+		}
+	});
 	
+	// "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg",
 	
 
 	return {

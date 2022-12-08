1252a27b205a896186be38784f82c61edc5c5cd5
Agustin Daniel Messina <agustindmessina@gmail.com>
2022-12-08 10:12:13 -0300

    meta tags

--- components/content/layouts/Layout.tsx
+++ components/content/layouts/Layout.tsx
@@ -8,25 +8,38 @@ interface Props {
 }
 
 
+const origin = (typeof window ==='undefined') ? '' : window.location.origin
+
+
 
 export const Layout: FC<PropsWithChildren & Props> = ({ children, title }) => {
-  return (
-		<>
-			<Head>
-				<title>{ title || "Pokemon App"}</title>
-				<meta name="author" content="Agustin Messina" />
-				<meta name="description" content={`Informacion sobre el pokemon ${title}`} />
-				<meta name="keywords" content={`${title}, pokemon, pokedex`} />
-			</Head>
 
-            <Navbar />
 
-            <main style={{
-				padding: '0px 20px'
-			}}>
-                { children }
-            </main>
 
-		</>
-	);
+
+
+	return (
+			<>
+				<Head>
+					<title>{ title || "Pokemon App"}</title>
+
+					<meta property="og:title" content={`Informacion sobre ${title}`} />
+					<meta property="og:description" content={`Esta es la pagina sobre ${title}`} />
+					<meta property="og:image" content={`${origin}/img/banner.png`} />
+
+					<meta name="author" content="Agustin Messina" />
+					<meta name="description" content={`Informacion sobre el pokemon ${title}`} />
+					<meta name="keywords" content={`${title}, pokemon, pokedex`} />
+				</Head>
+
+				<Navbar />
+
+				<main style={{
+					padding: '0px 20px'
+				}}>
+					{ children }
+				</main>
+
+			</>
+		);
 }
